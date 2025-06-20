73d73d6008add96f4ff718ff174809677b4b0fc2
safwan-kher <eng.safwan.kherallah@gmail.com>
2025-06-20 12:15:01 +0200

    Book Entity is added

--- src/main/java/com/example/library/domain/model/Book.java
+++ src/main/java/com/example/library/domain/model/Book.java
@@ -1,15 +1,30 @@
 package com.example.library.domain.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 // In Spring Boot:
 // jakarta.validation.*: this is a package from Oracle, we used the dependency to access it.
 
 /**
  * Core domain entity representing a Book.
  */
+
+@Data // generates getters, setters, toString, equals and hashCode
+@NoArgsConstructor // required by JPA
+@AllArgsConstructor // optional, but useful
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotBlank(message = "Title is required")
